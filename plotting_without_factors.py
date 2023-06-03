4c9a0d3733a29d5d1f58c4bb1c7c7ad212f45ed7
amaliitpkd <133376670+amaliitpkd@users.noreply.github.com>
2023-06-03 14:54:01 +0530

    Update plotting_without_factors.py
    added a function simulate which takes various factors affecting the virus spread

--- plotting_without_factors.py
+++ plotting_without_factors.py
@@ -15,6 +15,21 @@ class Place:
     def __init__(self, cmn_place_id):
         self.cmn_place_id = id
         self.num_sick = 0
+def simulate(places, n_healthy, n_sick, iterations,social_distancing_factor , vaccination_factor):
+    town = [Place(i) for i in range(places)]
+    people = [Person('healthy', 0) for i in range(n_healthy)] + \
+             [Person('sick', 0) for i in range(n_sick)]
+
+    vaccinated_count = int((n_healthy + n_sick) * vaccination_factor)
+    for i in range(vaccinated_count):
+        people[i].vaccination = True
+
+    healthy_history = [n_healthy]
+    sick_history = [n_sick]
+    recovered_history = [0]
+    dead_history = [0]
+    days = [day for day in range(iterations)]
+
 
 
 
