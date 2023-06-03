1cfc40620c40ae248f8ce4a39f44e3b89c77b954
Durgasri2005 <135050612+Durgasri2005@users.noreply.github.com>
2023-06-03 14:47:24 +0530

    Update plotting_without_factors.py
    created a class Person and added its attributes

--- plotting_without_factors.py
+++ plotting_without_factors.py
@@ -3,7 +3,11 @@ from matplotlib import pyplot as plt
 
 
 class Person:
-    def __init__(self, status):
+    def __init__(self, status, travel_rate):
         self.status = status
         self.time_sick = 0
         self.cmn_place = []
+        self.travel_rate = travel_rate
+        self.vaccination = False
+
+
